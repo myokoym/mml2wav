14daa9ee57e07309519289b28a04bb531facb381
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-04-26 01:12:12 +0900

    Add --sampling_rate option

--- lib/mml2wav/command.rb
+++ lib/mml2wav/command.rb
@@ -12,14 +12,15 @@ module Mml2wav
     def initialize(arguments)
       @options = parse_options(arguments)
       @output_path = @options[:output] || "doremi.wav"
+      @sampling_rate = @options[:sampling_rate] || 8000
       @sounds = arguments.join(" ")
     end
 
     def run
-      format = WaveFile::Format.new(:mono, :pcm_8, 8000)
+      format = WaveFile::Format.new(:mono, :pcm_8, @sampling_rate)
       @sine_waves = {}
       WaveFile::Writer.new(@output_path, format) do |writer|
-        buffer_format = WaveFile::Format.new(:mono, :float, 8000)
+        buffer_format = WaveFile::Format.new(:mono, :float, @sampling_rate)
         @sounds.split(//).each do |sound|
           frequency = Scale::FREQUENCIES[sound.downcase.to_sym] || 0
           samples = (@sine_waves[sound] ||= sine_wave(frequency))
@@ -39,6 +40,10 @@ module Mml2wav
       parser.on("--output=FILE", "Specify output file path") do |path|
         options[:output] = path
       end
+      parser.on("--sampling_rate=RATE",
+                "Specify sampling rate", Integer) do |rate|
+        options[:sampling_rate] = rate
+      end
       parser.parse!(arguments)
 
       if arguments.empty?
@@ -49,13 +54,13 @@ module Mml2wav
       options
     end
 
-    def sine_wave(frequency, rate=8000, sec=0.1, velocity=0.5)
-      max = rate * sec
+    def sine_wave(frequency, sec=0.1, velocity=0.5)
+      max = @sampling_rate * sec
       if frequency == 0
         return Array.new(max) { 0.0 }
       end
       0.upto(max).collect do |n|
-        Math.sin(2.0 * Math::PI * n / rate * frequency) * velocity
+        Math.sin(2.0 * Math::PI * n / @sampling_rate * frequency) * velocity
       end
     end
   end
