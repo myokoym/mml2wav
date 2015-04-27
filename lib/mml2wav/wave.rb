ed27b68664308fbda4338fecb3f392004e9c8052
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-04-27 23:14:51 +0900

    Support multi channels such as stereo

--- lib/mml2wav/wave.rb
+++ lib/mml2wav/wave.rb
@@ -6,15 +6,52 @@ module Mml2wav
     class << self
       include WaveFile
 
-      def write(sounds, options={})
+      def write(soundses, options={})
+        if soundses.is_a?(String)
+          n_channels = :mono
+          soundses = [soundses]
+          size = 1
+        elsif soundses.size == 1
+          n_channels = :mono
+          size = 1
+        elsif soundses.size == 2
+          n_channels = :stereo
+          size = soundses.size
+        else
+          n_channels = soundses.size
+          size = soundses.size
+        end
         output_path = options[:output] || "doremi.wav"
         sampling_rate = options[:sampling_rate] || 22050
 
-        format = Format.new(:mono, :pcm_8, sampling_rate)
+        format = Format.new(n_channels, :pcm_8, sampling_rate)
         Writer.new(output_path, format) do |writer|
-          buffer_format = Format.new(:mono, :float, sampling_rate)
-          parser = Parser.new(sounds, sampling_rate, options)
-          while samples = parser.wave!
+          buffer_format = Format.new(n_channels, :float, sampling_rate)
+          parsers = []
+          soundses.each do |sounds|
+            parsers << Parser.new(sounds, sampling_rate, options)
+          end
+          waves = Array.new(parsers.size) { [] }
+          loop do
+            parsers.each_with_index do |parser, i|
+              wave = parser.wave!
+              waves[i] += wave if wave
+            end
+            break if waves.all? {|wave| wave.empty? }
+            buffer_size = waves.reject {|wave| wave.empty? }.collect(&:size).min
+            break unless buffer_size
+            samples = []
+            buffer_size.times do
+              sample = []
+              waves.each do |wave|
+                if wave.first
+                  sample << wave.shift
+                else
+                  sample << 0.0
+                end
+              end
+              samples << sample
+            end
             buffer = Buffer.new(samples, buffer_format)
             writer.write(buffer)
           end
