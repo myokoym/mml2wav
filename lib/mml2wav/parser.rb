44b9b2385710639a1abcbd46cdc122e410f1ac43
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-05-04 02:14:14 +0900

    Support tie

--- lib/mml2wav/parser.rb
+++ lib/mml2wav/parser.rb
@@ -3,7 +3,7 @@ require "mml2wav/scale"
 module Mml2wav
   class Parser
     def initialize(sounds, sampling_rate, options={})
-      pattern = /T\d+|V\d+|L\d+|[A-GR][#+-]?\d*\.?|O\d+|[><]|./i
+      pattern = /T\d+|V\d+|L\d+|[A-GR][#+-]?\d*\.?(?:&[A-GR][#+-]?\d*\.?)*|O\d+|[><]|./i
       @sounds = sounds.scan(pattern)
       @sampling_rate = sampling_rate
       @bpm = options[:bpm] || 120
@@ -26,10 +26,23 @@ module Mml2wav
           @velocity = $1.to_i
         when /\AL(\d+)/i
           @default_length = $1.to_f
-        when /\A([A-GR][#+-]?)(\d+)?(\.)?/i
+        when /\A([A-GR][#+-]?)(\d+)?(\.)?((?:&[A-GR][#+-]?\d*\.?)*)/i
           sound = $1
           length = $2.to_f if $2
           length /= 1.5 if $3
+          sec = base_sec / length / @bpm
+          if $4
+            $4.scan(/&[A-GR][#+-]?(\d+)?(\.)?/i).each do |len, dot|
+              if len
+                sub_length = len.to_f
+              else
+                sub_length = @default_length
+              end
+              sub_length /= 1.5 if dot
+              sub_sec = base_sec / sub_length / @bpm
+              sec += sub_sec
+            end
+          end
         when /\AO(\d+)/i
           @octave = $1.to_i
         when "<"
@@ -37,7 +50,6 @@ module Mml2wav
         when ">"
           @octave -= @octave_reverse ? -1 : 1
         end
-        sec = base_sec / length / @bpm
         amplitude = @velocity.to_f / 10
         frequency = Scale::FREQUENCIES[sound.downcase]
         next unless frequency
